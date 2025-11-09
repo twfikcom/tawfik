3d0f4693df31b54a9985b7535b095224638623ad
Tawfiq Attia <tawfiqabulhag@gmail.com>
2025-11-09 18:33:56 +0200

    Fix animations, loading screen, and translation file paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/twfikcom/',
+  base: '/tawfik/',
   root: '.',
   build: {
     outDir: 'dist',
